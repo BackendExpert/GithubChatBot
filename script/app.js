6951c25bea5a1594f6b0fc258d3f9fd94de4d6e7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-05 19:11:39 +0530

    Update app.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- script/app.js
+++ script/app.js
@@ -1,9 +1,12 @@
 document.getElementById('searchForm').addEventListener('submit', function(e) {
     e.preventDefault();
     const searchQuery = document.getElementById('searchQuery').value;
-    // searchRepositories(searchQuery);
-    
+    // searchRepositories(searchQuery);    
     alert(searchQuery)
 });
+
+async function GithubRepo(searchQuery){
+    
+}
 
 // https://api.github.com/search/repositories?q=
