58ee7ffa9e8d08fd23548631f7e7274947f62b4d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-05 19:22:16 +0530

    Update app.js
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- script/app.js
+++ script/app.js
@@ -6,7 +6,20 @@ document.getElementById('searchForm').addEventListener('submit', function(e) {
 });
 
 async function GithubRepo(searchQuery){
-    
+    const repo_api = `https://api.github.com/search/repositories?q=${encodeURIComponent(query)}`
+
+    try {
+        const res = await fetch(repo_api)
+        if(res.ok){
+        //    const data = await res.json()
+        //    return data.items; 
+           return await res.json(data.items);
+        }
+    }
+    catch (err){
+        console.log('Error :', err)
+        return null
+    }
 }
 
 // https://api.github.com/search/repositories?q=
