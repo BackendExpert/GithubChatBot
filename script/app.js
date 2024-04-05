aac16da449aa7c4dcdf3b3d2c5a53cfbd32be272
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-04-05 19:32:19 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

---
+++ script/app.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('searchForm');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    // get value of input
+    const searchData = document.forms['FormSearch']['searchQuery'].value;
+
+    if(searchData === ""){
+        alert("Enter Value")
+    }
+})
